c2186998105922bc3ec3035592b15760c292e54d
Beyondo <58893646+Beyondo@users.noreply.github.com>
2022-12-10 16:24:22 +0300

    Update colab.py

--- colab.py
+++ colab.py
@@ -4,10 +4,10 @@ def init(model_name):
         print("No GPU found. Go to Runtime -> Change runtime type, and choose \"GPU\" then click Save.")
     else:
         print("Running on -> ", end="")
-        print(torch.cuda.get_device_name("cuda:0"), end=".\nInstalling dependencies -> ")
-        print("Done.\nUsing model -> ", end="")
+        print(torch.cuda.get_device_name("cuda:0") + ".")
+        print("Initializing model -> " + model_name + ":")
         from diffusers import StableDiffusionPipeline
         import sys
         sys.stdout = open('stdout.txt', 'w')
         pipe = StableDiffusionPipeline.from_pretrained(model_name, revision="fp16", torch_dtype=torch.float16)
-        print(model_name, end=".\n")
+        print("Done.")
