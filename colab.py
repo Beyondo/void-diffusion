4331f0e1d1b2d4c6ef92d113acee6a40459a7f6f
Beyondo <58893646+Beyondo@users.noreply.github.com>
2022-12-10 17:28:34 +0300

    Update colab.py

--- colab.py
+++ colab.py
@@ -1,9 +1,9 @@
 import torch
 model_name = ""
 ready = False
-pipe = None
+text2img = None
 def init(ModelName):
-    global model_name, ready, pipe
+    global model_name, ready, text2img, img2img, inpaint
     model_name = ModelName
     if not torch.cuda.is_available():
         print("No GPU found. Go to Runtime -> Change runtime type, and choose \"GPU\" then click Save.")
@@ -11,8 +11,10 @@ def init(ModelName):
         print("Running on -> ", end="")
         print(torch.cuda.get_device_name("cuda:0") + ".")
         print("Initializing model -> " + model_name + ":")
-        from diffusers import StableDiffusionPipeline
-        pipe = StableDiffusionPipeline.from_pretrained(model_name, revision="fp16", torch_dtype=torch.float16)
+        from diffusers import StableDiffusionPipeline, StableDiffusionImg2ImgPipeline, StableDiffusionInpaintPipeline
+        text2img = StableDiffusionPipeline.from_pretrained(model_name, revision="fp16", torch_dtype=torch.float16)
+        img2img = StableDiffusionImg2ImgPipeline(**text2img.components)
+        inpaint = StableDiffusionInpaintPipeline(**text2img.components)
         print("Done.")
         ready = True
         from IPython.display import clear_output; clear_output()
