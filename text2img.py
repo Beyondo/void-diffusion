bebc390d6fef7769e8ad996ec6aab84856faf3d8
Beyondo <58893646+Beyondo@users.noreply.github.com>
2022-12-10 19:17:10 +0300

    Update text2img.py

--- text2img.py
+++ text2img.py
@@ -2,11 +2,11 @@ import torch
 import colab
 import os
 def process(seed, positive_prompt, negative_prompt, guidance_scale, save_to_google_drive, directory):
-    genSeed = torch.random.seed() if Seed == 0 else Seed
+    genSeed = torch.random.seed() if seed == 0 else seed
     generator = torch.Generator("cuda").manual_seed(genSeed)
-    image = colab.text2img(Positive, guidance_scale=Guidance_Scale, generator=generator).images[0]
-    if SaveToGoogleDrive:
-        dir = '/content/gdrive/MyDrive/' + Directory
+    image = colab.text2img(positive_prompt, guidance_scale=guidance_scale, generator=generator).images[0]
+    if save_to_google_drive:
+        dir = '/content/gdrive/MyDrive/' + directory
         if not os.path.exists(dir): os.makedirs(dir)
         imgSavePath = "%s/voidops-%d.png" % (dir, genSeed)
         image.save(imgSavePath)
