1b57ee26d9082a59c06c0d9945e849c454193879
Beyondo <58893646+Beyondo@users.noreply.github.com>
2022-12-10 16:20:17 +0300

    Update colab.py

--- colab.py
+++ colab.py
@@ -1,24 +1,13 @@
-import importlib
-importlib.reload(diffusers)
-import subprocess
-def install(name):
-    subprocess.call(['pip3', 'install', name])
-install("torch")
 import torch
-def init():
+def init(model_name):
     if not torch.cuda.is_available():
         print("No GPU found. Go to Runtime -> Change runtime type, and choose \"GPU\" then click Save.")
     else:
         print("Running on -> ", end="")
         print(torch.cuda.get_device_name("cuda:0"), end=".\nInstalling dependencies -> ")
-        install("einops torch transformers diffusers accelerate > /dev/null")
         print("Done.\nUsing model -> ", end="")
-        Model = "CompVis/stable-diffusion-v1-4" # @param ["CompVis/stable-diffusion-v1-4", "hakurei/waifu-diffusion"]
-        # @markdown **Available models**:<br>
-        # @markdown `CompVis/stable-diffusion-v1-4` -> Trained on everything<br>
-        # @markdown `hakurei/waifu-diffusion` -> Trained on anime<br>
         from diffusers import StableDiffusionPipeline
         import sys
         sys.stdout = open('stdout.txt', 'w')
-        pipe = StableDiffusionPipeline.from_pretrained(Model, revision="fp16", torch_dtype=torch.float16)
-        print(Model, end=".\n")
+        pipe = StableDiffusionPipeline.from_pretrained(model_name, revision="fp16", torch_dtype=torch.float16)
+        print(model_name, end=".\n")
