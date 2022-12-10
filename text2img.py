5bdd50a274827fb2c5662b2e6d367e1b5e61bb1d
Beyondo <58893646+Beyondo@users.noreply.github.com>
2022-12-10 19:08:51 +0300

    Create text2img.py

---
+++ text2img.py
@@ -0,0 +1,13 @@
+import torch
+import colab
+import os
+def process():
+    genSeed = torch.random.seed() if Seed == 0 else Seed
+    generator = torch.Generator("cuda").manual_seed(genSeed)
+    image = colab.text2img(Positive, guidance_scale=Guidance_Scale, generator=generator).images[0]
+    if SaveToGoogleDrive:
+        dir = '/content/gdrive/MyDrive/' + Directory
+        if not os.path.exists(dir): os.makedirs(dir)
+        imgSavePath = "%s/voidops-%d.png" % (dir, genSeed)
+        image.save(imgSavePath)
+        print("Saved to " + imgSavePath)
