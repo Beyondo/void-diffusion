37f3dce36cc8140312d66fdd76f1f2124e8add8e
Beyondo <58893646+Beyondo@users.noreply.github.com>
2022-12-10 19:53:26 +0300

    Update patcher.py

--- patcher.py
+++ patcher.py
@@ -1,6 +1,7 @@
-import shutil, sys
+import shutil, sys, importlib
 patch_path = '/content/void-diffusion/safety_checker_patched.py'
 def patch():
     python_version = "python%d.%d" % (sys.version_info.major, sys.version_info.minor)
     target_path = '/usr/local/lib/%s/dist-packages/diffusers/pipelines/stable_diffusion/safety_checker.py' % python_version
     shutil.copyfile(patch_path, target_path)
+    importlib.invalidate_caches()
