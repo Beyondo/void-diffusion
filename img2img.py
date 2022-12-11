806d06095e6b52d725aba17e9369cd40c30893a8
Beyondo <58893646+Beyondo@users.noreply.github.com>
2022-12-11 23:01:08 +0300

    Added img2img

--- img2img.py
+++ img2img.py
@@ -1,11 +1,16 @@
 import torch, os, time, datetime, colab, postprocessor, importlib
 from IPython.display import Image
 from IPython.display import display
+
+import requests
+from PIL import Image
+from io import BytesIO
 importlib.reload(postprocessor)
 def process(ShouldSave):
     # Load image
-    image = Image.open(colab.settings['ImageURL'])
-    display(image) # temporary
+    response = requests.get(colab.settings['ImageURL'])
+    init_image = Image.open(BytesIO(response.content)).convert('RGB')
+    display(init_image)
     # Process image
     colab.settings['Seed'] = torch.random.seed() if colab.settings['Seed'] == 0 else colab.settings['Seed']
     generator = torch.Generator("cuda").manual_seed(colab.settings['Seed'])
@@ -13,9 +18,12 @@ def process(ShouldSave):
         width=colab.settings['Width'],
         height=colab.settings['Height'],
         prompt=colab.settings['Prompt'],
+        image=init_image,
         negative_prompt=colab.settings['NegativePrompt'],
         guidance_scale=colab.settings['GuidanceScale'],
         num_inference_steps=colab.settings['Steps'],
-        generator=generator).images[0]
+        generator=generator
+        callback=progress.callback,
+        callback_steps=5).images[0]
     if ShouldSave:
         postprocessor.save_gdrive(image)
