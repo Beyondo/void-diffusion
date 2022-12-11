a2bbf11713a65dcf3c6242c3763cf7bfbdd44aef
Beyondo <58893646+Beyondo@users.noreply.github.com>
2022-12-11 19:47:40 +0300

    Update postprocessor.py

--- postprocessor.py
+++ postprocessor.py
@@ -2,19 +2,19 @@ import torch, os, time, datetime, colab
 from IPython.display import Image
 from IPython.display import display
 
-def get_save_name(filename):
+def get_save_path(filename):
     dir = '/content/gdrive/MyDrive/' + colab.save_directory
     if not os.path.exists(dir): os.makedirs(dir)
     return "%s/%d" % (dir, filename)
 
 def save_gdrive(img, filename):
-    imgSavePath = get_save_name()
+    imgSavePath = get_save_path(filename)
     imgFile = imgSavePath + ".png"
     img.save(imgFile)
     return imgFile.replace("/content/gdrive/MyDrive/", "")
 
 def save_settings(filename):
-    imgSavePath = get_save_name()
+    imgSavePath = get_save_path(filename)
     settingsFile = imgSavePath + "-settings.txt"
     if colab.save_settings:
         with open(settingsFile, "w") as f:
@@ -23,5 +23,5 @@ def save_settings(filename):
     return settingsFile.replace("/content/gdrive/MyDrive/", "")
 
 def post_process(img, filename):
-    imgSavePath = get_save_name()
+    imgSavePath = get_save_path(filename)
     imgFile = imgSavePath + "-2x.png"
