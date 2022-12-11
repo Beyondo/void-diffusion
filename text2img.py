c9c644a252a7601c083bfafaee6950ddc96c4dee
Beyondo <58893646+Beyondo@users.noreply.github.com>
2022-12-11 04:11:00 +0300

    Update text2img.py

--- text2img.py
+++ text2img.py
@@ -8,8 +8,8 @@ def process(seed, positive_prompt, negative_prompt, guidance_scale, inference_st
     if save_to_google_drive:
         dir = '/content/gdrive/MyDrive/' + directory
         if not os.path.exists(dir): os.makedirs(dir)
-        imgSavePath = "%s/%d-voidops" % (dir, int(time.mktime(datetime.now().timetuple())))
+        imgSavePath = "%s/%d-voidops" % (dir, int(time.mktime(datetime.datetime.now().timetuple())))
         image.save(imgSavePath + ".png")
-        print("Saved to " + imgSavePath)
         display(image)
+        print("Saved to " + imgSavePath)
         return image
