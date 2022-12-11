dedb553c1f7fdd8b67b3d05dbf8546648afb4962
Beyondo <58893646+Beyondo@users.noreply.github.com>
2022-12-11 22:13:31 +0300

    Update text2img.py

--- text2img.py
+++ text2img.py
@@ -3,16 +3,12 @@ from IPython.display import Image
 from IPython.display import display
 importlib.reload(postprocessor)
 def processing_callback(iter, t, latents):
-    # convert latents to image
     with torch.no_grad():
         latents = 1 / 0.18215 * latents
         image = colab.text2img.vae.decode(latents).sample
         image = (image / 2 + 0.5).clamp(0, 1)
-        # we always cast to float32 as this does not cause significant overhead and is compatible with bfloa16
         image = image.cpu().permute(0, 2, 3, 1).float().numpy()
-        # convert to PIL Images
         image = colab.text2img.numpy_to_pil(image)
-        # do something with the Images
         for i, img in enumerate(image):
             display(img)
 
