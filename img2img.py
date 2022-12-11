b73f12944f65ffaeed3331f7ed7ca46794c78ff9
Beyondo <58893646+Beyondo@users.noreply.github.com>
2022-12-11 23:05:15 +0300

    Update img2img.py

--- img2img.py
+++ img2img.py
@@ -10,17 +10,17 @@ def process(ShouldSave):
     # Load image
     response = requests.get(colab.settings['ImageURL'])
     init_image = Image.open(BytesIO(response.content)).convert('RGB')
+    init_image.thumbnail((colab.settings['Width'], colab.settings['Height']))
     display(init_image)
     # Process image
     colab.settings['Seed'] = torch.random.seed() if colab.settings['Seed'] == 0 else colab.settings['Seed']
     generator = torch.Generator("cuda").manual_seed(colab.settings['Seed'])
     image = colab.img2img(
-        width=colab.settings['Width'],
-        height=colab.settings['Height'],
         prompt=colab.settings['Prompt'],
         image=init_image,
         negative_prompt=colab.settings['NegativePrompt'],
         guidance_scale=colab.settings['GuidanceScale'],
+        strength=colab.settings['Strength'],
         num_inference_steps=colab.settings['Steps'],
         generator=generator,
         callback=progress.callback,
