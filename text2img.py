eb5e00670a53fc4e16a4ccacd057754daca5ad25
Beyondo <58893646+Beyondo@users.noreply.github.com>
2022-12-11 18:44:17 +0300

    Update text2img.py

--- text2img.py
+++ text2img.py
@@ -1,7 +1,7 @@
 import torch, os, time, datetime, colab, postprocessor
 from IPython.display import Image
 from IPython.display import display
-def process():
+def process(ShouldSave):
     genSeed = torch.random.seed() if colab.settings['Seed'] == 0 else colab.settings['Seed']
     generator = torch.Generator("cuda").manual_seed(genSeed)
     image = colab.text2img(
@@ -12,6 +12,6 @@ def process():
         guidance_scale=colab.settings['GuidanceScale'],
         num_inference_steps=colab.settings['Steps'],
         generator=generator).images[0]
-    if colab.settings['SaveToGoogleDrive']:
+    if save:
         postprocessor.save_image(image)
     
