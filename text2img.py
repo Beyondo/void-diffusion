2ac79df5a83615d0daab3c05fca0fa1ba810c628
Beyondo <58893646+Beyondo@users.noreply.github.com>
2022-12-11 23:01:20 +0300

    Increased preview speed

--- text2img.py
+++ text2img.py
@@ -1,16 +1,5 @@
-import torch, os, time, datetime, colab, postprocessor, importlib
-from IPython.display import Image
-from IPython.display import display
+import torch, os, time, datetime, colab, postprocessor, progress, importlib
 importlib.reload(postprocessor)
-def processing_callback(iter, t, latents):
-    with torch.no_grad():
-        latents = 1 / 0.18215 * latents
-        image = colab.text2img.vae.decode(latents).sample
-        image = (image / 2 + 0.5).clamp(0, 1)
-        image = image.cpu().permute(0, 2, 3, 1).float().numpy()
-        image = colab.text2img.numpy_to_pil(image)
-        for i, img in enumerate(image):
-            display(img, display_id=str(i))
 
 def process(ShouldSave):
     if 'Seed' not in colab.settings: "Please set your settings first."
@@ -25,8 +14,8 @@ def process(ShouldSave):
         guidance_scale=colab.settings['GuidanceScale'],
         num_inference_steps=colab.settings['Steps'],
         generator=generator,
-        callback=processing_callback,
-        callback_steps=10).images[0]
+        callback=progress.callback,
+        callback_steps=5).images[0]
     timestamp = int(time.mktime(datetime.datetime.now().timetuple()))
     if ShouldSave:
         if colab.save_settings: postprocessor.save_settings(timestamp)
