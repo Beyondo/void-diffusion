6a9327eac6ca4eb8e8c962d058b45059176542b1
Beyondo <58893646+Beyondo@users.noreply.github.com>
2022-12-11 18:36:02 +0300

    Update text2img.py

--- text2img.py
+++ text2img.py
@@ -2,7 +2,7 @@ import torch, os, time, datetime, colab, postprocessor
 from IPython.display import Image
 from IPython.display import display
 def process(width, height, seed, positive_prompt, negative_prompt, guidance_scale, inference_steps, save_to_google_drive, directory):
-    print ("Generating image using seed: " + str(colab.settings['seed']) + "...")
+    print ("Generating image using seed: " + str(colab.settings['Seed']) + "...")
     genSeed = torch.random.seed() if seed == 0 else seed
     generator = torch.Generator("cuda").manual_seed(genSeed)
     image = colab.text2img(
