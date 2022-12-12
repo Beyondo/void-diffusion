b095dc0c7bc810b314a4bfe12f2f2606b5dcceaa
Beyondo <58893646+Beyondo@users.noreply.github.com>
2022-12-12 21:34:06 +0300

    progress lag fix

--- progress.py
+++ progress.py
@@ -2,14 +2,13 @@ import torch, colab, time
 from IPython.display import display
 last_image_time = 0
 def callback(iter, t, latents):
-    with torch.no_grad():
-        latents = 1 / 0.18215 * latents
-        images = colab.text2img.vae.decode(latents).sample
-        images = (images / 2 + 0.5).clamp(0, 1)
-        images = images.cpu().permute(0, 2, 3, 1).float().numpy()
-        images = colab.text2img.numpy_to_pil(images)
-        # print image every 2 seconds
-        if time.time() - last_image_time > 2:
-            colab.last_image_time = time.time()
+    if time.time() - last_image_time > 2:
+        colab.last_image_time = time.time()
+        with torch.no_grad():
+            latents = 1 / 0.18215 * latents
+            images = colab.text2img.vae.decode(latents).sample
+            images = (images / 2 + 0.5).clamp(0, 1)
+            images = images.cpu().permute(0, 2, 3, 1).float().numpy()
+            images = colab.text2img.numpy_to_pil(images)
             display("Seed: %d" % colab.get_current_image_seed(), display_id=colab.get_current_image_uid() + "_seed")
             display(images[0], display_id=colab.get_current_image_uid())
