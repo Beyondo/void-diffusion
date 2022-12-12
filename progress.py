46241677205f5c00d67bd06814865e7ecdd9de1d
Beyondo <58893646+Beyondo@users.noreply.github.com>
2022-12-12 21:04:00 +0300

    Time-based preview

--- progress.py
+++ progress.py
@@ -1,5 +1,6 @@
-import torch, colab
+import torch, colab, time
 from IPython.display import display
+last_image_time = 0
 def callback(iter, t, latents):
     with torch.no_grad():
         latents = 1 / 0.18215 * latents
@@ -7,6 +8,8 @@ def callback(iter, t, latents):
         images = (images / 2 + 0.5).clamp(0, 1)
         images = images.cpu().permute(0, 2, 3, 1).float().numpy()
         images = colab.text2img.numpy_to_pil(images)
-        
-        display("Seed: %d" % colab.get_current_image_seed(), display_id=colab.get_current_image_uid() + "_seed")
-        display(images[0], display_id=colab.get_current_image_uid())
+        # print image every 2 seconds
+        if time.time() - last_image_time > 2:
+            colab.last_image_time = time.time()
+            display("Seed: %d" % colab.get_current_image_seed(), display_id=colab.get_current_image_uid() + "_seed")
+            display(images[0], display_id=colab.get_current_image_uid())
