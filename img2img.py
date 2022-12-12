887fdc8b69dc7f2b802548686bbb5e47dba52928
Beyondo <58893646+Beyondo@users.noreply.github.com>
2022-12-12 03:56:40 +0300

    Update img2img.py

--- img2img.py
+++ img2img.py
@@ -1,4 +1,4 @@
-import torch, os, time, datetime, colab, postprocessor, progress, importlib
+import torch, os, time, datetime, colab, postprocessor, progress, importlib, random
 from IPython.display import Image
 from IPython.display import display
 
@@ -10,7 +10,7 @@ def process(ShouldSave):
     if 'Seed' not in colab.settings:
         print("Please set your settings first.")
         return
-    colab.settings['Seed'] = torch.random.seed(int(time.mktime(datetime.datetime.now().timetuple()))) if colab.settings['Seed'] == 0 else colab.settings['Seed']
+    colab.settings['Seed'] = random.getrandbits(int(time.mktime(datetime.datetime.now().timetuple()))) if colab.settings['Seed'] == 0 else colab.settings['Seed']
     generator = torch.Generator("cuda").manual_seed(colab.settings['Seed'])
     # Load image
     response = requests.get(colab.settings['ImageURL'])
