bbf6f2304914b9826092f7e4c51453cdbc38a7df
Beyondo <58893646+Beyondo@users.noreply.github.com>
2022-12-12 17:53:47 +0300

    Number of iterations update

--- text2img.py
+++ text2img.py
@@ -6,7 +6,7 @@ def process(ShouldSave, ShouldPreview = True):
     timestamp = int(time.mktime(datetime.datetime.now().timetuple()))
     if ShouldSave and colab.save_settings: postprocessor.save_settings(timestamp, mode="text2img")
     num_iterations = colab.settings['Iterations']
-    display("Iterations: 0/%d" % (i / num_iterations), display_id="iterations")
+    display("Iterations: 0/%d" % num_iterations, display_id="iterations")
     for i in range(num_iterations):
         colab.image_id = i # needed for progress.py
         print("Seed: %d" % colab.get_current_image_seed())
