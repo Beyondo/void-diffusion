f744f87fadac899e635f26a1eb746f833bb9940d
Beyondo <58893646+Beyondo@users.noreply.github.com>
2022-12-14 16:25:51 +0300

    Update server.py

--- server.py
+++ server.py
@@ -4,18 +4,4 @@ import time
 from IPython import get_ipython
 try:
   port = int(sys.argv[1])
-  class TCPHandler(socketserver.BaseRequestHandler):
-    def handle(self):
-        # self.request is the TCP socket connected to the client
-        self.data = self.request.recv(1024).strip()
-        print("{} wrote:".format(self.client_address[0]))
-        print(self.data)
-        # just send back the same data, but upper-cased
-        self.request.sendall(self.data.upper())
-  if __name__ == "__main__":
-      HOST, PORT = "localhost", port
-      with socketserver.TCPServer((HOST, PORT), TCPHandler) as server:
-          # Activate the server; this will keep running until you
-          # interrupt the program with Ctrl-C
-          server.serve_forever()
-except Exception as e: print("Error: %s" % e)
+  get_ipython().system_raw('python3 -m http.server {} &'.format(port))
