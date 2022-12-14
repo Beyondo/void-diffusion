5dc46844131e9af846779f42af7902abb1632637
Beyondo <58893646+Beyondo@users.noreply.github.com>
2022-12-14 16:27:26 +0300

    Update server.py

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ import time
 from IPython import get_ipython
 try:
   port = int(sys.argv[1])
-  get_ipython().system_raw('python3 -m http.server {} &'.format(port))
+  get_ipython().system('python3 -m http.server {} &'.format(port))
 except Exception as e:
   print("Server error: ", e)
   sys.exit(1)
