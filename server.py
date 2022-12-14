6ecd4d79142cc35482ec705116f457e2e071dc47
Beyondo <58893646+Beyondo@users.noreply.github.com>
2022-12-14 16:28:47 +0300

    Update server.py

--- server.py
+++ server.py
@@ -3,9 +3,10 @@ import socketserver
 import time
 from IPython import get_ipython
 from IPython.display import Javascript
+import os
 try:
   port = int(sys.argv[1])
-  get_ipython().system('python3 -m http.server {} &'.format(port))
+  os.system("python -m http.server " + str(port))
 except Exception as e:
   print("Server error: ", e)
   sys.exit(1)
