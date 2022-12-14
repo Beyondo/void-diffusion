c367c45134f101ec761acef3c22d339fb405e8d6
Beyondo <58893646+Beyondo@users.noreply.github.com>
2022-12-14 16:27:49 +0300

    Update server.py

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ import sys
 import socketserver
 import time
 from IPython import get_ipython
+from IPython.display import Javascript
 try:
   port = int(sys.argv[1])
   get_ipython().system('python3 -m http.server {} &'.format(port))
