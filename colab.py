22c4d93b40292a6a7746e003fce0a3914a3e2d11
Beyondo <58893646+Beyondo@users.noreply.github.com>
2022-12-15 17:40:43 +0300

    Update colab.py

--- colab.py
+++ colab.py
@@ -6,7 +6,6 @@ from diffusers.schedulers import PNDMScheduler, LMSDiscreteScheduler, DDIMSchedu
 from transformers import CLIPFeatureExtractor, CLIPModel, CLIPTokenizer, CLIPTextModel, CLIPTextConfig
 import ClipGuided
 from diffusers.pipelines.stable_diffusion import StableDiffusionPipelineOutput
-torch.set_default_dtype(torch.float16)
 model_name = ""
 ready = False
 tokenizer = None
@@ -63,9 +62,9 @@ def init(ModelName):
             #importlib.reload(VOIDPipeline)
             #VOIDPipeline.Take_Over()
             torch.set_default_dtype(torch.float16)
-            config = CLIPTextConfig.from_pretrained("openai/clip-vit-large-patch14", torch_dtype=torch.float16)
+            config = CLIPTextConfig.from_pretrained("openai/clip-vit-base-patch16", torch_dtype=torch.float16)
             config.max_position_embeddings = 512
-            tokenizer = CLIPTokenizer.from_pretrained("openai/clip-vit-large-patch14", torch_dtype=torch.float16)
+            tokenizer = CLIPTokenizer.from_pretrained("openai/clip-vit-base-patch16", torch_dtype=torch.float16)
             tokenizer.model_max_length = 512
             pipeline = StableDiffusionPipeline.from_pretrained(model_name, revision=rev, torch_dtype=torch.float16).to("cuda:0")
             pipeline.text_encoder = CLIPTextModel(config).to("cuda:0")
