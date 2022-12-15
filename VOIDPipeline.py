07a3a0ec8ed3eb79a196b2c08402c97d8da12560
Beyondo <58893646+Beyondo@users.noreply.github.com>
2022-12-15 06:38:41 +0300

    Update VOIDPipeline.py

--- VOIDPipeline.py
+++ VOIDPipeline.py
@@ -50,23 +50,9 @@ def text2img_encode_prompt(self, prompt, device, num_images_per_prompt, do_class
         truncation=True,
         return_tensors="pt",
     )
-    text_input_ids = text_inputs.input_ids
-    untruncated_ids = self.tokenizer(prompt, padding="longest", return_tensors="pt").input_ids
-    if untruncated_ids.shape[-1] >= text_input_ids.shape[-1] and not torch.equal(text_input_ids, untruncated_ids):
-        removed_text = self.tokenizer.batch_decode(untruncated_ids[:, self.tokenizer.model_max_length - 1 : -1])
-        logger.warning(
-            "The following part of your input was truncated because CLIP can only handle sequences up to"
-            f" {self.tokenizer.model_max_length} tokens: {removed_text}"
-        )
-    if hasattr(self.text_encoder.config, "use_attention_mask") and self.text_encoder.config.use_attention_mask:
-        attention_mask = text_inputs.attention_mask.to(device)
-    else:
-        attention_mask = None
-    text_embeddings = self.text_encoder(
-        text_input_ids.to(device),
-        attention_mask=attention_mask,
-        max_length=self.tokenizer.model_max_length,
-    )
+    print("Text inputs: ", text_inputs)
+    text_embeddings = self.text_encoder(text_inputs.input_ids.to(self.device))[0]
+    print("Text embeddings: ", text_embeddings)
     text_embeddings = text_embeddings[0]
     # duplicate text embeddings for each generation per prompt, using mps friendly method
     bs_embed, seq_len, _ = text_embeddings.shape
