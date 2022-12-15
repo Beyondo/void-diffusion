4577fd56016ff061ef772a7fe5655b39bf4c9ce8
Beyondo <58893646+Beyondo@users.noreply.github.com>
2022-12-16 00:55:21 +0300

    Update colab.py

--- colab.py
+++ colab.py
@@ -48,17 +48,17 @@ def create_guided_pipeline(pipeline):
 def modify_clip_limit(limit):
     global pipeline
     # Text Encoder
+    old_embeddings = pipeline.text_encoder.text_model.embeddings
     old_weights = pipeline.text_encoder.text_model.embeddings.position_embedding.weight.data.to("cuda:0")
     pipeline.text_encoder.config.max_position_embeddings = limit
     # Bug: The following line is supposed to be a hack to make the model reload everything using the new config but it makes the model generate random images
     pipeline.text_encoder.text_model.__init__(config=pipeline.text_encoder.config)
     #
     pipeline.text_encoder.text_model.to("cuda:0")
-    pipeline.text_encoder.text_model.embeddings.position_embedding = torch.nn.Embedding(limit, 768).to("cuda:0")
     pipeline.text_encoder.text_model.embeddings.position_embedding.weight.data[:old_weights.shape[0]] = old_weights
     # Tokenizer
-    #spipeline.tokenizer.model_max_length = limit
-    #pipeline.text_encoder.resize_token_embeddings(len(pipeline.tokenizer))
+    pipeline.tokenizer.model_max_length = limit
+    pipeline.text_encoder.resize_token_embeddings(len(pipeline.tokenizer))
     
 def init(ModelName):
     global model_name, ready, pipeline, tokenizer, text2img, img2img, inpaint
