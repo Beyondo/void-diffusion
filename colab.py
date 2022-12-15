485d3b3a478a918f91999e66b1c4c448bcf55ec2
Beyondo <58893646+Beyondo@users.noreply.github.com>
2022-12-15 06:47:10 +0300

    Update colab.py

--- colab.py
+++ colab.py
@@ -67,6 +67,7 @@ def init(ModelName):
             pipeline.tokenizer = tokenizer
             pipeline.text_encoder = CLIPTextModel.from_pretrained("openai/clip-vit-base-patch32", torch_dtype=torch.float16).to("cuda:0")
             pipeline.text_encoder.resize_token_embeddings(len(tokenizer))
+            print(len(tokenizer))
             pipeline.text_encoder.load_state_dict(pipeline.text_encoder.state_dict())
             pipeline.text_encoder.resize_token_embeddings(512)
             text2img = StableDiffusionPipeline(**pipeline.components)
