52679934ef7eb7b78f8877d7720b435d4bc674cd
Beyondo <58893646+Beyondo@users.noreply.github.com>
2022-12-15 09:08:23 +0300

    Update colab.py

--- colab.py
+++ colab.py
@@ -62,9 +62,9 @@ def init(ModelName):
             #importlib.reload(VOIDPipeline)
             #VOIDPipeline.Take_Over()
             # CLIPTextConfig
-            config = CLIPTextConfig.from_pretrained("openai/clip-vit-base-patch16")
+            config = CLIPTextConfig.from_pretrained("openai/clip-vit-base-patch32")
             config.max_position_embeddings = 512
-            tokenizer = CLIPTokenizer.from_pretrained("openai/clip-vit-base-patch16")
+            tokenizer = CLIPTokenizer.from_pretrained("openai/clip-vit-base-patch32")
             tokenizer.model_max_length = 512
             pipeline = StableDiffusionPipeline.from_pretrained(model_name).to("cuda:0")
             pipeline.text_encoder = CLIPTextModel(config).to("cuda:0")
