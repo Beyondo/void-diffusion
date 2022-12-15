2c1e589a5acc91dda2e47c6b7a74464a9129428e
Beyondo <58893646+Beyondo@users.noreply.github.com>
2022-12-15 07:31:45 +0300

    Update VOIDPipeline.py

--- VOIDPipeline.py
+++ VOIDPipeline.py
@@ -178,6 +178,7 @@ def generate_text2img(
     with self.progress_bar(total=num_inference_steps) as progress_bar:
         for i, t in enumerate(timesteps):
             # expand the latents if we are doing classifier free guidance
+            latent_model_input = latent_model_input.to(torch.float16)
             latent_model_input = torch.cat([latents] * 2) if do_classifier_free_guidance else latents
             latent_model_input = self.scheduler.scale_model_input(latent_model_input, t)
             # predict the noise residual
