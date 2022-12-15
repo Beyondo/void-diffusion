0c342c3fb81e34c0750cca0d5dcf1dbaf659c8d4
Beyondo <58893646+Beyondo@users.noreply.github.com>
2022-12-15 20:43:59 +0300

    Update colab.py

--- colab.py
+++ colab.py
@@ -68,7 +68,7 @@ def init(ModelName):
             # -> Because the text encoder is not trained on the same dataset as the image encoder.
             torch.set_default_dtype(torch.float16)
             pipeline = StableDiffusionPipeline.from_pretrained(model_name, revision=rev).to("cuda:0")
-            print(pipeline.text_encoder)
+            print(pipeline.text_encoder.text_model.embeddings)
             pipeline.text_encoder.config.max_position_embeddings = 512
             pipeline.text_encoder.position_embeddings = torch.nn.Embedding(512, 768)
             pipeline.tokenizer.model_max_length = 512
