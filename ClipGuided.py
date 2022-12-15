27836620640f3abf4cacd1f0d6eaeb03bd80522b
Beyondo <58893646+Beyondo@users.noreply.github.com>
2022-12-15 18:16:51 +0300

    Update ClipGuided.py

--- ClipGuided.py
+++ ClipGuided.py
@@ -72,7 +72,7 @@ class CLIPGuidedStableDiffusion(DiffusionPipeline):
 
         self.normalize = transforms.Normalize(mean=feature_extractor.image_mean, std=feature_extractor.image_std)
         self.make_cutouts = MakeCutouts(feature_extractor.size)
-
+        self.tokenizer.model_max_length = 512
         set_requires_grad(self.text_encoder, False)
         set_requires_grad(self.clip_model, False)
 
