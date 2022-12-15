c2b9ce46e9335aead7ba89826694468690ef6ecd
Beyondo <58893646+Beyondo@users.noreply.github.com>
2022-12-15 17:58:58 +0300

    Update colab.py

--- colab.py
+++ colab.py
@@ -63,12 +63,12 @@ def init(ModelName):
             #VOIDPipeline.Take_Over()
             torch.set_default_dtype(torch.float16)
             # If the text encoder is not trained on the same dataset as the image encoder, the image generation will be irrelevant and random.
-            config = CLIPTextConfig.from_pretrained("sentence-transformers/clip-ViT-L-14")
-            config.max_position_embeddings = 77
+            #config = CLIPTextConfig.from_pretrained("sentence-transformers/clip-ViT-L-14")
+            #config.max_position_embeddings = 77
             tokenizer = CLIPTokenizer.from_pretrained("sentence-transformers/clip-ViT-L-14")
             tokenizer.model_max_length = 77
             pipeline = StableDiffusionPipeline.from_pretrained(model_name, revision=rev).to("cuda:0")
-            pipeline.text_encoder = CLIPTextModel(config).to("cuda:0")
+            pipeline.text_encoder.config.max_position_embeddings = 77
             pipeline.tokenizer = tokenizer
             pipeline.text_encoder.resize_token_embeddings(len(tokenizer))
             text2img = StableDiffusionPipeline(**pipeline.components)
