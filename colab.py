68226ef217aac422a269af7507a0ef4d0b3560fc
Beyondo <58893646+Beyondo@users.noreply.github.com>
2022-12-15 09:45:56 +0300

    Update colab.py

--- colab.py
+++ colab.py
@@ -65,7 +65,7 @@ def init(ModelName):
             # I think it's because the model is in fp16, but the bias is in fp32.
             #config.max_position_embeddings = 512
             tokenizer = CLIPTokenizer.from_pretrained("openai/clip-vit-base-patch16", torch_dtype=torch.float16)
-            #tokenizer.model_max_length = 512
+            tokenizer.model_max_length = 512
             pipeline = StableDiffusionPipeline.from_pretrained(model_name, revision=rev, torch_dtype=torch.float16).to("cuda:0")
             pipeline.tokenizer = tokenizer
             pipeline.text_encoder.resize_token_embeddings(len(tokenizer))
