8d7fec7c6074b6eb1f7beaf57a079bdea6abcfef
Beyondo <58893646+Beyondo@users.noreply.github.com>
2022-12-15 23:33:09 +0300

    Update colab.py

--- colab.py
+++ colab.py
@@ -49,15 +49,19 @@ def modify_clip_limit(limit):
     global pipeline
     if limit < 77:
         print("You cannot reduce the limit below 77 because we need to keep the CLIP text encoder weights.")
+    # Tokenizer
+    pipeline.tokenizer.model_max_length = limit
+    pipeline.text_encoder.resize_token_embeddings(len(pipeline.tokenizer))    
     # Text Encoder
     # Resize position embedding without losing the old weights (unless you like random noise)
     old_embedding = pipeline.text_encoder.text_model.embeddings.position_embedding #.weight.data
     pipeline.text_encoder.text_model.embeddings.position_embedding = torch.nn.Embedding(limit, 768).to("cuda:0")
     pipeline.text_encoder.text_model.embeddings.position_embedding.weight.data[:old_embedding.weight.data.shape[0]] = old_embedding.weight.data
     pipeline.text_encoder.config.max_position_embeddings = limit
-    # Tokenizer
-    pipeline.tokenizer.model_max_length = limit
-    pipeline.text_encoder.resize_token_embeddings(len(pipeline.tokenizer))    
+    # Recreate the text encoder
+    pipeline.text_encoder.text_model = CLIPTextModel(CLIPTextConfig(**pipeline.text_encoder.config.to_dict())).to("cuda:0")
+    pipeline.text_encoder.text_model.load_state_dict(pipeline.text_encoder.state_dict())
+    pipeline.text_encoder.resize_token_embeddings(len(pipeline.tokenizer))
     
 def init(ModelName):
     global model_name, ready, pipeline, tokenizer, text2img, img2img, inpaint
