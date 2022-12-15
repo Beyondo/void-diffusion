57b5c147adb48578dcc01902e1b64b7152cfa11c
Beyondo <58893646+Beyondo@users.noreply.github.com>
2022-12-15 20:55:17 +0300

    Update colab.py

--- colab.py
+++ colab.py
@@ -67,7 +67,7 @@ def init(ModelName):
             # Why does it generate an image that has nothing to do with the text?
             # -> Because the text encoder is not trained on the same dataset as the image encoder.
             torch.set_default_dtype(torch.float16)
-            pipeline = StableDiffusionPipeline.from_pretrained("https://github.com/acheong08/stable-diffusion-v1-5", revision=rev).to("cuda:0")
+            pipeline = StableDiffusionPipeline.from_pretrained(model_name, revision=rev).to("cuda:0")
             print(pipeline.text_encoder.text_model.embeddings)
             pipeline.text_encoder.config.max_position_embeddings = 512
             pipeline.text_encoder.position_embeddings = torch.nn.Embedding(512, 768)
@@ -82,6 +82,9 @@ def init(ModelName):
             #from IPython.display import clear_output; clear_output()
             display.display(HTML("Model <strong><span style='color: green'>%s</span></strong> has been selected." % model_name))
         except Exception as e:
+            # if contains "502 Server Error"
+            if "502" in str(e):
+                print("Received 502 Server Error: Huggingface is currently down." % model_name)
             print("Failed to initialize model %s with error %s" % (model_name, e))
 
 def prepare(mode):
