6ebff09fe17ec26ae14c311925d28ab2ffeb8faa
Beyondo <58893646+Beyondo@users.noreply.github.com>
2022-12-15 23:39:03 +0300

    Update colab.py

--- colab.py
+++ colab.py
@@ -59,7 +59,7 @@ def modify_clip_limit(limit):
     pipeline.text_encoder.text_model.embeddings.position_embedding.weight.data[:old_embedding.weight.data.shape[0]] = old_embedding.weight.data
     pipeline.text_encoder.config.max_position_embeddings = limit
     # Recreate the text encoder
-    pipeline.text_encoder.text_model.__init__(self=pipeline.text_encoder.text_model, config=pipeline.text_encoder.config)
+    pipeline.text_encoder.text_model.__init__(config=pipeline.text_encoder.config)
 def init(ModelName):
     global model_name, ready, pipeline, tokenizer, text2img, img2img, inpaint
     ready = False
