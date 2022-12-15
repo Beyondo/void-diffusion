7cc66a808d82111c7653bb400cd6a66d76f0bcbe
Beyondo <58893646+Beyondo@users.noreply.github.com>
2022-12-15 21:36:58 +0300

    Update colab.py

--- colab.py
+++ colab.py
@@ -70,7 +70,7 @@ def init(ModelName):
             #pipeline.text_encoder.text_model.embeddings.position_embedding = torch.nn.Embedding(512, 768).to("cuda:0")
             #pipeline.text_encoder.config.max_position_embeddings = 512
             #pipeline.text_encoder.config.model_max_length = 512
-            pipeline.text_encoder = CLIPTextModel(CLIPTextConfig(**pipeline.text_encoder.config.to_dict())).to("cuda:0")
+            pipeline.text_encoder = CLIPTextModel(pipeline.text_encoder.config).to("cuda:0")
             #pipeline.tokenizer.model_max_length = 512
             #pipeline.text_encoder.resize_token_embeddings(len(pipeline.tokenizer))
             ###############################
