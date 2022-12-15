da61b27413b554f6f158aea7c78ea633bb2ad546
Beyondo <58893646+Beyondo@users.noreply.github.com>
2022-12-15 09:25:42 +0300

    Update colab.py

--- colab.py
+++ colab.py
@@ -68,7 +68,7 @@ def init(ModelName):
             #tokenizer.model_max_length = 512
             pipeline = StableDiffusionPipeline.from_pretrained(model_name, revision=rev, torch_dtype=torch.float16).to("cuda:0")
             #pipeline.text_encoder = CLIPTextModel(config).to("cuda:0")
-            #pipeline.tokenizer = tokenizer
+            pipeline.tokenizer = tokenizer
             #pipeline.text_encoder.resize_token_embeddings(len(tokenizer))
             text2img = StableDiffusionPipeline(**pipeline.components)
             img2img = StableDiffusionImg2ImgPipeline(**pipeline.components)
