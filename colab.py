8286f0ceb9e8903d4844d217478c6a71e033ee45
Beyondo <58893646+Beyondo@users.noreply.github.com>
2022-12-15 23:21:52 +0300

    Update colab.py

--- colab.py
+++ colab.py
@@ -62,7 +62,6 @@ def modify_clip_limit(limit):
     pipeline.vae.config.max_position_embeddings = limit
     # Unet
     pipeline.unet.config.max_position_embeddings = limit
-    pipeline.unet.resize_token_embeddings(len(pipeline.tokenizer))
 
     
     
