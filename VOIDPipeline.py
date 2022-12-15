2e8000c63fb6534a16357c692b3ceefe95079196
Beyondo <58893646+Beyondo@users.noreply.github.com>
2022-12-15 06:57:27 +0300

    Update VOIDPipeline.py

--- VOIDPipeline.py
+++ VOIDPipeline.py
@@ -34,7 +34,8 @@ def text2img_encode_prompt(self, prompt, device, num_images_per_prompt, do_class
         truncation=True,
         return_tensors="pt",
     )
-    text_embeddings = self.text_encoder(text_inputs.input_ids.to(self.device))[0]
+    print("Embed....")
+    text_embeddings = self.text_encoder(text_inputs.input_ids.to("cuda:0"))[0]
     print("Text embeddings: ", text_embeddings)
     text_embeddings = text_embeddings[0]
     # duplicate text embeddings for each generation per prompt, using mps friendly method
