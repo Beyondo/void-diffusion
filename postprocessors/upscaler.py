ded084f318095d722df223f238394024f7a747a3
Beyondo <58893646+Beyondo@users.noreply.github.com>
2022-12-17 02:46:39 +0300

    Update upscaler.py

--- postprocessors/upscaler.py
+++ postprocessors/upscaler.py
@@ -19,7 +19,7 @@ def upscale(upscaler, scale, image_input_path):
         image = image.resize((image.width * scale, image.height * scale), PIL.Image.BICUBIC)
     elif upscaler.lower() == "gfpgan":
         # Copy image to inputs/upload.png
-        image.save("inputs/upload.png")
+        image.save("temp/input/upload.png")
         IPython.get_ipython().system("python inference_gfpgan.py -i temp/input -o temp/output -v 1.3.8 -s " + str(scale) + " --bg_upsampler realesrgan")
         image = PIL.Image.open("temp/output.png")
     # Convert back to PIL image
