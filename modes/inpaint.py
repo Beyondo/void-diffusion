83cfc6407ca13c80fb6aa6e22a8ce34acae61823
Beyondo <58893646+Beyondo@users.noreply.github.com>
2022-12-17 15:39:38 +0300

    Update inpaint.py

--- modes/inpaint.py
+++ modes/inpaint.py
@@ -20,27 +20,22 @@ def process(ShouldSave, ShouldPreview = True):
     mask_image.thumbnail((colab.settings['Width'], colab.settings['Height']))
     mask_applied_image = Image.new("RGB", init_image.size)
     mask_applied_image.paste(init_image, (0, 0), mask_image)
-    latents = None
-    if True: #colab.settings["Strength"] > 0:
-        # generate random image latents for inpainting
-        latents = torch.randn(1, 18, 512, device="cuda")
-        latent_image = torch.nn.functional.interpolate(
-            colab.generator.synthesis(latents, noise_mode="const"),
-            size=init_image.size,
-            mode="bilinear",
-            align_corners=False)[0].cpu().clamp_(0, 1).permute(1, 2, 0).numpy()
-        import numpy as np
-        latent_image = Image.fromarray((latent_image * 255).astype(np.uint8))
-        grid = colab.image_grid([init_image, mask_image, mask_applied_image, latent_image], 1, 4)
-    else:
-        grid = colab.image_grid([init_image, mask_image, mask_applied_image, latent_image], 1, 3)
-    display(grid)
+    display(colab.image_grid([init_image, mask_image, mask_applied_image], 1, 3))
     # Process image
     for i in range(num_iterations):
         colab.image_id = i # needed for progress.py
         generator = torch.Generator("cuda").manual_seed(colab.settings['InitialSeed'] + i)
         progress.reset()
         progress.show()
+        latents = None
+        if True: #colab.settings["Strength"] > 0:
+            # generate random image latents for inpainting
+            latents = torch.randn(1, 18, 512, device="cuda")
+            latent_image = torch.nn.functional.interpolate(
+                generator.synthesis(latents, noise_mode="const"),
+                size=init_image.size,
+                mode="bilinear",
+                align_corners=False)[0].cpu().clamp_(0, 1).permute(1, 2, 0).numpy()
         image = colab.inpaint(
             prompt=colab.settings['Prompt'],
             image=init_image,
