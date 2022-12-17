709c9c09cceed8e7bc0db12e0e055899e467f7da
Beyondo <58893646+Beyondo@users.noreply.github.com>
2022-12-17 15:25:06 +0300

    Update inpaint.py

--- modes/inpaint.py
+++ modes/inpaint.py
@@ -18,10 +18,20 @@ def process(ShouldSave, ShouldPreview = True):
     init_image.thumbnail((colab.settings['Width'], colab.settings['Height']))
     mask_image = Image.open(BytesIO(requests.get(colab.settings['MaskImageURL']).content)).convert('RGBA')
     mask_image.thumbnail((colab.settings['Width'], colab.settings['Height']))
-    # Create an image called "mask_applied" which is basically the initial image with the mask applied to it, the mask is black and white, so we can just multiply the two images together
-    mask_applied = Image.new("RGB", init_image.size)
-    mask_applied.paste(init_image, (0, 0), mask_image)
-    grid = colab.image_grid([init_image, mask_image, mask_applied], 1, 3)
+    mask_applied_image = Image.new("RGB", init_image.size)
+    mask_applied_image.paste(init_image, (0, 0), mask_image)
+    latents = None
+    if True: #colab.settings["Strength"] > 0:
+        # generate latent space from the mask using torch
+        latent = torch.randn(1, 512, device="cuda")
+        # generate a mask from the mask image
+        mask = torch.tensor(mask_image, device="cuda").float() / 255
+        mask = mask.permute(2, 0, 1).unsqueeze(0)
+        # apply the mask to the latent space
+        latent_image = latent * mask + (1 - mask) * 0.5
+        grid = colab.image_grid([init_image, mask_image, mask_applied_image, latent_image], 1, 4)
+    else:
+        grid = colab.image_grid([init_image, mask_image, mask_applied_image, latent_image], 1, 3)
     display(grid)
     # Process image
     for i in range(num_iterations):
@@ -37,6 +47,7 @@ def process(ShouldSave, ShouldPreview = True):
             guidance_scale=colab.settings['GuidanceScale'],
             num_inference_steps=colab.settings['Steps'],
             generator=generator,
+            latents=latents,
             callback=progress.callback if ShouldPreview else None,
             callback_steps=20).images[0]
         progress.show(image)
