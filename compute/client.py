78e2459eee789d67daddd9dabe7f50130fc94c8f
Beyondo <58893646+Beyondo@users.noreply.github.com>
2022-12-19 19:06:21 +0300

    Update client.py

--- compute/client.py
+++ compute/client.py
@@ -1,16 +1,18 @@
 import requests, json, threading, time, colab
 from IPython.display import display
 from IPython.display import HTML
+from IPython.display import clear_output;
 # import get_ipython()
 from IPython import get_ipython
 UniqueID = "VOID-d07f0ae2-c61b-46d4-a157-8c5dc6ea5657"
 def job(data):
     pass
 def run(uuid):
+    clear_output()
     while True:
         response = requests.post("https://voidops.com/diffusion/api.php", json = {"type": "get_jobs", "uuid": uuid}, headers={"User-Agent": "VOID-Compute-Client"})
         if response.status_code == 200:
-            display(HTML("Connected to %s" % uuid), display_id = "void-connection")
+            display(HTML("<font color='green'>Working for %s</font>" % uuid), display_id = "void-connection")
             if response.text != "":
               data = json.loads(response.text)
               if data["status"] == "ok":
