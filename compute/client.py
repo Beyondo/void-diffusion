a90848066c56ae49aa9dbda274f4bbd68ddedd02
Beyondo <58893646+Beyondo@users.noreply.github.com>
2022-12-19 19:22:47 +0300

    Update client.py

--- compute/client.py
+++ compute/client.py
@@ -7,11 +7,11 @@ from IPython import get_ipython
 UniqueID = "VOID-d07f0ae2-c61b-46d4-a157-8c5dc6ea5657"
 def job(data):
     pass
-def set_connection_status(uuid, msg, color):
-    display(HTML("%s <code><font color='%s'>%s</font></code><br>*************************************************************************<br>" % (msg, color, uuid)),  display_id = "void-connection")
+def set_connection_status(uuid, msg, color, end = ""):
+    display(HTML("%s <code><font color='%s'>%s</font></code>%s<br>*************************************************************************<br>" % (msg, color, uuid, end)),  display_id = "void-connection")
 def run(uuid):
     clear_output()
-    set_connection_status(uuid, "Waiting for", "orange")
+    set_connection_status(uuid, "Connecting to", "orange", "...")
     while True:
         response = requests.post("https://voidops.com/diffusion/api.php", json = {"type": "get_jobs", "uuid": uuid}, headers={"User-Agent": "VOID-Compute-Client"})
         if response.status_code == 200:
@@ -38,5 +38,5 @@ def run(uuid):
                     # display message in red text
                     display(HTML("<font color='red'>" + data["message"] + "</font>"), display_id = "void-error")
         else:
-            set_connection_status(uuid, "Waiting for", "orange")
+            set_connection_status(uuid, "Waiting for", "orange", "...")
         time.sleep(1)
