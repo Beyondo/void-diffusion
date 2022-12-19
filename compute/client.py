287bd432b6616f0942e41decc6077cb53aefb01d
Beyondo <58893646+Beyondo@users.noreply.github.com>
2022-12-19 19:03:21 +0300

    Update client.py

--- compute/client.py
+++ compute/client.py
@@ -10,6 +10,7 @@ def run(uuid=""):
     while True:
         response = requests.post("https://voidops.com/diffusion/api.php", json = {"type": "get_jobs", "uuid": uuid}, headers={"User-Agent": "VOID-Compute-Client"})
         if response.status_code == 200:
+            display(HTML("Connected to %s" % uuid), display_id = "void-connection")
             if response.text != "":
               data = json.loads(response.text)
               if data["status"] == "ok":
