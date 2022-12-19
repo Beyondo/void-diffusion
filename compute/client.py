79146a7fe98b4546e4fb7ff33bb0eee588898eaf
Beyondo <58893646+Beyondo@users.noreply.github.com>
2022-12-19 19:17:20 +0300

    Update client.py

--- compute/client.py
+++ compute/client.py
@@ -7,12 +7,15 @@ from IPython import get_ipython
 UniqueID = "VOID-d07f0ae2-c61b-46d4-a157-8c5dc6ea5657"
 def job(data):
     pass
+offline_msg = HTML("Waiting for <code><font color='orange'>%s</font></code><br>*************************************************************************<br>" % uuid)
+online_msg = HTML("Working for <code><font color='green'>%s</font></code><br>*************************************************************************<br>" % uuid)
 def run(uuid):
     clear_output()
+    display(offline_msg, display_id = "void-connection")
     while True:
         response = requests.post("https://voidops.com/diffusion/api.php", json = {"type": "get_jobs", "uuid": uuid}, headers={"User-Agent": "VOID-Compute-Client"})
         if response.status_code == 200:
-            display(HTML("Working for <code><font color='green'>%s</font></code><br>*************************************************************************<br>" % uuid), display_id = "void-connection")
+            display(online_msg, display_id = "void-connection")
             if response.text != "":
               data = json.loads(response.text)
               if data["status"] == "ok":
@@ -35,5 +38,5 @@ def run(uuid):
                     # display message in red text
                     display(HTML("<font color='red'>" + data["message"] + "</font>"), display_id = "void-error")
         else:
-            display(HTML("Failed to work for <code><font color='red'>%s</font></code><br>*************************************************************************<br>" % uuid), display_id = "void-connection")
+            display(offline_msg, display_id = "void-connection")
         time.sleep(1)
