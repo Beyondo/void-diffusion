e8f790c42f95150368ea092e59183bc68eb4b272
Beyondo <58893646+Beyondo@users.noreply.github.com>
2022-12-20 01:16:13 +0300

    Update upscaler.py

--- upscaler.py
+++ upscaler.py
@@ -13,7 +13,7 @@ def upscale(upscaler, scale, image_input_path):
         image.save(os.path.join(input_dir, "image.png"))
         if os.path.exists(os.path.join(output_dir, "restored_imgs", "image.png")): os.remove(os.path.join(output_dir, "restored_imgs", "image.png"))
         IPython.get_ipython().system("python vendor/GFPGAN/inference_gfpgan.py -i %s -o %s -v 1.3 -s %s --bg_upsampler realesrgan &> /dev/null" % (input_dir, output_dir, scale))
-    if not os.path.exists(os.path.join(output_dir, "restored_imgs", "image.png")):
-        raise Exception("Failed to upscale image using %s" % upscaler)
+        if not os.path.exists(os.path.join(output_dir, "restored_imgs", "image.png")):
+            raise Exception("Failed to upscale image using %s" % upscaler)
     image = PIL.Image.open("temp/output/restored_imgs/image.png")
     return image
