249c61dc9cb9fc2a258a2ba5c61e6447ead02ccd
Beyondo <58893646+Beyondo@users.noreply.github.com>
2022-12-25 02:33:09 +0300

    Compute update

--- compute/client.py
+++ compute/client.py
@@ -2,73 +2,46 @@ import requests, json, threading, time, os, colab
 from IPython.display import display
 from IPython.display import HTML
 from IPython.display import clear_output;
+import job_manager
 import importlib
 # import get_ipython()
 API = "https://voidops.com/diffusion/api.php"
 from IPython import get_ipython
-def process_job(job):
-    print("Processing job " + job["id"])
-    if job['type'] == "run_script":
-        try:
-            importlib.import_module(os.path.join("scripts", job['script']))
-        except Exception as e:
-            job['status'] = "error"
-            job['progress'] = -1
-            print(e)
-            return False
-    elif job['type'] == "install_vendor":
-        try:
-            import env
-            env.install_vendor()
-        except Exception as e:
-            job['status'] = "error"
-            job['progress'] = -1
-            print(e)
-            return False
-    else:
-        job['status'] = "error"
-        job['progress'] = -1
-        print("Unknown job type")
-        return False
-    return True
 def set_connection_status(uuid, msg, color, end = ""):
     display(HTML("%s <code><font color='%s'>%s</font></code>%s<br><hr><br>" % (msg, color, uuid, end)),  display_id = "void-connection")
-def update_job(job):
-    response = requests.post(API, json = {"uuid": uuid, "job": job, "type": "update_job"}, headers={"User-Agent": "VOID-Compute-Client"})
+def send(data):
+    response = requests.post(API, json = data, headers={"User-Agent": "VOID-Compute-Client"})
     if response.status_code == 200:
         r = json.loads(response.text)
         if r["status"] == "ok":
             return True
         else:
-            print("Job update refused: " + r["message"])
+            print("Job submission refused: " + r["message"])
     else:
-        print("Couldn't post job update: " + str(response))
+        print("Couldn't post job submission: " + str(response))
     return False
+
+
 def run(uuid):
     clear_output()
     set_connection_status(uuid, "Connecting to", "orange", "...")
+    job_manager.run()
     while True:
-        response = requests.post(API, json = {"type": "get_jobs", "uuid": uuid}, headers={"User-Agent": "VOID-Compute-Client"})
+        response = send({"uuid": uuid, "type": "get_jobs"})
         if response.status_code == 200:
             if response.text != "":
               r = json.loads(response.text)
               if r["status"] == "ok":
                   set_connection_status(uuid, "Currently working for ", "green")
-                  num_jobs = len(r["jobs"])
+                  server_jobs = r["jobs"]
+                  num_jobs = len(server_jobs)
                   if num_jobs > 0:
+                    for job in job_manager.currently_running:
+                        if not any(server_job["id"] == job.data.id for server_job in server_jobs): # if job is not in server jobs
+                            job_manager.signal_termination(job)
                     for job in r["jobs"]:
                         if job['status'] == "pending":
-                            # Send a post request to the server
-                            job['status'] = "processing"
-                            job['progress'] = 0
-                            if update_job(job) and process_job(job):
-                                job['status'] = "complete"
-                                job['progress'] = 100
-                                update_job(job)
-                            else:
-                                job['status'] = "error"
-                                job['progress'] = -1
-                                update_job(job)
+                            job_manager.add_to_queue(job)
               else:
                 if r["code"] != 404:
                     display(HTML("<font color='red'>" + r["message"] + "</font>"), display_id = "void-error")
---
+++ compute/job.py
@@ -0,0 +1,55 @@
+import json, os, importlib, client, job_manager
+class job:
+    def __init__(self, uuid, jobData):
+        self.uuid = uuid
+        self.data = jobData
+
+    def submit(self):
+        self.data.progress = 100
+        self.data.status = "complete"
+        return client.send({"uuid": self.uuid, "job": self.data, "type": "submit_job"})
+
+    def update(self):
+        return client.send({"uuid": self.uuid, "job": self.data, "type": "update_job"})
+
+    def callback(self, progress, status):
+        self.data.progress = progress
+        self.data.status = status
+        self.update()
+
+    def process(self):
+        if self.update():
+            job_manager.currently_running.append(self)
+            print("Processing job " + self.data["id"])
+            if self.data['type'] == "run_script":
+                try:
+                    importlib.import_module(os.path.join("scripts", self.data['script']))
+                except Exception as e:
+                    self.data['status'] = "error"
+                    self.data['progress'] = -1
+                    print(e)
+                    return False
+            elif self.data['type'] == "install_vendor":
+                try:
+                    import env
+                    env.install_vendor()
+                except Exception as e:
+                    self.data['status'] = "error"
+                    self.data['progress'] = -1
+                    print(e)
+                    return False
+            else:
+                self.data['status'] = "error"
+                self.data['progress'] = -1
+                print("Unknown job type")
+                return False
+            self.data['status'] = "complete"
+            self.data['progress'] = 100
+        else:
+            self.data['status'] = "error"
+            self.data['progress'] = -1
+        self.update()
+
+    def stop():
+        self.data['status'] = "stopped"
+        self.update()
