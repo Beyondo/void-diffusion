f168320219e8d7105812bcb70b8798536b9c93e9
Beyondo <58893646+Beyondo@users.noreply.github.com>
2022-12-30 02:59:58 +0300

    Update inpaint.py

--- legacy/inpaint.py
+++ legacy/inpaint.py
@@ -23,8 +23,6 @@ def process(ShouldSave, maxNumJobs, ShouldPreview = True, ReplaceResult = True):
         init_image.thumbnail((colab.settings['Width'], colab.settings['Height']))
         mask_image = Image.open(BytesIO(requests.get(colab.settings['MaskImageURL']).content)).convert("RGB")
         init_image.thumbnail((colab.settings['Width'], colab.settings['Height']))
-        init_image = init_image.resize((colab.settings['Width'], colab.settings['Height']))
-        mask_image = mask_image.resize((colab.settings['Width'], colab.settings['Height']))
         mask_applied_image = Image.blend(init_image, mask_image, 0.5)
         display(colab.image_grid([init_image, mask_image, mask_applied_image], 1, 3))
         colab.image_size = init_image.size
