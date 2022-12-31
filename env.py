0ee5336fba8c112ded18f5dd6fe0bc8e166a5f3e
Beyondo <58893646+Beyondo@users.noreply.github.com>
2022-12-31 20:39:31 +0300

    Update env.py

--- env.py
+++ env.py
@@ -28,10 +28,10 @@ def install_vendor():
             IPython.get_ipython().system("python vendor/GFPGAN/inference_gfpgan.py -i vendor/GFPGAN/inputs/whole_imgs -o vendor/GFPGAN/results/whole_imgs -v 1.3 -s 2 --bg_upsampler realesrgan > /dev/null 2>&1")
         # Real-ESRGAN
         os.remove("vendor/Real-ESRGAN") if os.path.exists("vendor/Real-ESRGAN") else None
+        IPython.get_ipython().system("git clone https://github.com/xinntao/Real-ESRGAN.git vendor/Real-ESRGAN > /dev/null")
         os.chdir("vendor/Real-ESRGAN")
         IPython.get_ipython().system("wget https://github.com/xinntao/Real-ESRGAN/releases/download/v0.2.2.4/RealESRGAN_x4plus_anime_6B.pth -P weights > /dev/null")
         IPython.get_ipython().system("wget https://github.com/xinntao/Real-ESRGAN/releases/download/v0.2.1/RealESRGAN_x2plus.pth -p weights > /dev/null")
-        IPython.get_ipython().system("git clone https://github.com/xinntao/Real-ESRGAN.git vendor/Real-ESRGAN > /dev/null")
         IPython.get_ipython().system("pip install basicsr facexlib gfpgan ffmpeg-python > /dev/null")
         IPython.get_ipython().system("pip install -q -r vendor/Real-ESRGAN/requirements.txt > /dev/null")
         IPython.get_ipython().system("python vendor/Real-ESRGAN/setup.py develop > /dev/null")
