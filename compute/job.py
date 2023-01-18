4ca6baa4fb8f1c85e39cee60320fef30c43d98c6
Beyondo <58893646+Beyondo@users.noreply.github.com>
2023-01-18 10:48:19 +0300

    Update job.py

--- compute/job.py
+++ compute/job.py
@@ -21,12 +21,12 @@ class job:
     def process(self):
         if self.update():
             job_manager.running_jobs.append(self)
-            scriptFile = os.path.join(".scripts", self.data['script'] + ".py")
+            scriptFile = os.path.join("scripts", self.data['script'] + ".py")
             print(f"Processing in {scriptFile} ({self.data['id']})")
             try:
                 # print current working directory
                 print(os.getcwd())
-                importlib.import_module(scriptFile, package="scripts")
+                importlib.import_module(scriptFile, package=".")
             except Exception as e:
                 self.data['status'] = "error"
                 self.data['progress'] = -1
