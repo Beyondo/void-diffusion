c16fdefe13d2413cd97083d575471541179faca8
Beyondo <58893646+Beyondo@users.noreply.github.com>
2023-01-18 12:06:15 +0300

    a

--- job.py
+++ job.py
@@ -13,7 +13,6 @@ class job:
         job_manager.running_jobs.remove(self)
         self.data['progres'] = 100
         self.data['status'] = "complete"
-        print ("Job complete")
         return client.send("update_job", data={"uuid": self.uuid, "job": self.data })
     def signal_failure(self):
         job_manager.running_jobs.remove(self)
@@ -33,7 +32,7 @@ class job:
             self.signal_start()
             print(f"Processing in {self.data['script']} ({self.data['id']})")
             mod = importlib.import_module(self.data['script'])
-            #importlib.reload(mod)
+            importlib.reload(mod)
             if mod.run(args=self.data['args'], callback=self.callback):
                 self.signal_completion()
             else:
--- scripts/txt2img.py
+++ scripts/txt2img.py
@@ -1,4 +1,11 @@
 requirements = [ 'transformers' ]
+import time
 def run(args, callback):
-    print("running")
+    while True:
+        time.sleep(1)
+        callback(0, args['seeds'][0] + "Waiting...")
+        time.sleep(1)
+        callback(50, args['seeds'][0] + "Running...")
+        time.sleep(1)
+        callback(100, args['seeds'][0] + "Complete!")
     return True
