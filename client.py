4f15bfb858d267caef033700fcc78dd7ba607a9b
Beyondo <58893646+Beyondo@users.noreply.github.com>
2023-01-25 10:38:11 +0300

    Update client.py

--- client.py
+++ client.py
@@ -13,9 +13,7 @@ def set_connection_status(uuid, msg, color, end = ""):
     display(HTML("%s <code><font color='%s'>%s</font></code>%s<br><hr><br>" % (msg, color, uuid, end)),  display_id = "void-connection")
 def send(request_function, data):
     functionUrl = f"{API}/{request_function}"
-    print(data)
     response = requests.post(functionUrl, json = data, headers={"User-Agent": "VOID-Compute-Client"})
-    print(response.text)
     decoded = None
     if response.status_code == 200:
         if response.text == "":
