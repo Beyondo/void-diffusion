bf52c7da61d134f1043a4a7163ac72e7889a4268
Beyondo <58893646+Beyondo@users.noreply.github.com>
2023-04-08 21:17:16 +0300

    Update progress.py

--- legacy/progress.py
+++ legacy/progress.py
@@ -14,7 +14,20 @@ def show(img = None, iter = 0):
     global rendering_start_time
     image_id = colab.get_current_image_uid()
     seed = colab.get_current_image_seed()
-    display(HTML(f'<label>Seed: <code data-clipboard-text="{seed}">{seed}</code></label>'), display_id=image_id + "_seed")
+    seed_label = f'<label>Seed: <code id="seed">{seed}</code></label>'
+    copy_button = '<button onclick="copySeed()">Copy</button>'
+    javascript = f'''
+        function copySeed() {{
+            var seed = document.getElementById("seed");
+            var range = document.createRange();
+            range.selectNode(seed);
+            window.getSelection().removeAllRanges();
+            window.getSelection().addRange(range);
+            document.execCommand("copy");
+            window.getSelection().removeAllRanges();
+        }}
+    '''
+    display(HTML(seed_label + copy_button), display_id=image_id + "_seed")
     display(HTML("<label>Generation time: %.2fs</label>" % (time.time() - rendering_start_time)), display_id=image_id + "_time")
     display(HTML("<label>Original: Emerging...</label>"), display_id=image_id + "_original")
     display(HTML("<label>Saved: No</label>"), display_id=image_id + "_saved")
@@ -25,6 +38,8 @@ def show(img = None, iter = 0):
         if not replace_result: display("[Post-processed Image...]", display_id=image_id + "_image_scaled")
     else:
         display("...", display_id=image_id)
+    display(Javascript(javascript))
+
 def callback(iter, t, latents):
     global last_image_time
     if time.time() - last_image_time > 3:
