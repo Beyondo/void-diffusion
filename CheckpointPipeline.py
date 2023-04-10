4b0ccf1889d07a07a96ed0b8c36d391c03d1d948
Beyondo <58893646+Beyondo@users.noreply.github.com>
2023-04-10 06:50:23 +0300

    Update CheckpointPipeline.py

--- CheckpointPipeline.py
+++ CheckpointPipeline.py
@@ -1,10 +1,10 @@
 from diffusers.pipelines.stable_diffusion.convert_from_ckpt import load_pipeline_from_original_stable_diffusion_ckpt
 import torch
 def from_pretrained(checkpoint_path, is_img2img=False):
-    torch.set_default_dtype(torch.float16)
+    #torch.set_default_dtype(torch.float16)
     pipe = None
     try:
-        pipe = pipeline = load_pipeline_from_original_stable_diffusion_ckpt(
+        pipe = load_pipeline_from_original_stable_diffusion_ckpt(
         checkpoint_path,
         device='cuda',
         controlnet=None
