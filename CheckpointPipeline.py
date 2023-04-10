16caee4edfc716ac4b4960e9ba60bfa499c16b6b
Beyondo <58893646+Beyondo@users.noreply.github.com>
2023-04-10 07:31:22 +0300

    Checkpoint support

--- CheckpointPipeline.py
+++ CheckpointPipeline.py
@@ -1,4 +1,5 @@
-from diffusers.pipelines.stable_diffusion.convert_from_ckpt import load_pipeline_from_original_stable_diffusion_ckpt
+from convert_ckpt_to_diffusers import load_pipeline_from_original_stable_diffusion_ckpt
+
 import torch
 def from_pretrained(checkpoint_path, is_img2img=False):
     torch.set_default_dtype(torch.float32)
