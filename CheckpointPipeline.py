5d34103adf73ac01530cfc1ad35617b93b18eee9
Beyondo <58893646+Beyondo@users.noreply.github.com>
2023-04-10 08:11:21 +0300

    Ckpt Overhaul

--- CheckpointPipeline.py
+++ CheckpointPipeline.py
@@ -1,4 +1,4 @@
-from convert_ckpt_to_diffusers import load_pipeline_from_original_stable_diffusion_ckpt
+from diffusers.pipelines.stable_diffusion.convert_from_ckpt import load_pipeline_from_original_stable_diffusion_ckpt
 
 import torch
 def from_pretrained(checkpoint_path, is_img2img=False):
