3ba68250549be6a59d177877b8f78c83d3e796c2
Beyondo <58893646+Beyondo@users.noreply.github.com>
2023-04-10 05:22:51 +0300

    Update env.py

--- env.py
+++ env.py
@@ -24,7 +24,7 @@ def install_vendors():
         IPython.get_ipython().system("pip install realesrgan > /dev/null")
         # used for enhancing the background (non-face) regions
         IPython.get_ipython().system("wget https://github.com/TencentARC/GFPGAN/releases/download/v1.3.8/GFPGANv1.3.pth -P experiments/pretrained_models > /dev/null")
-        os.chdir("../..")
+        os.chdir("../../..")
         # Use vendor/GFPGAN/inference_gfpgan.py to download & cache the model
         if not os.path.exists("../vendor/GFPGAN/results/whole_imgs"):
             IPython.get_ipython().system("python vendor/GFPGAN/inference_gfpgan.py -i vendor/GFPGAN/inputs/whole_imgs -o vendor/GFPGAN/results/whole_imgs -v 1.3 -s 2 --bg_upsampler realesrgan > /dev/null 2>&1")
@@ -37,7 +37,7 @@ def install_vendors():
         IPython.get_ipython().system("pip install basicsr facexlib gfpgan ffmpeg-python > /dev/null")
         IPython.get_ipython().system("pip install -q -r requirements.txt > /dev/null")
         IPython.get_ipython().system("python setup.py develop > /dev/null")
-        os.chdir("../..")
+        os.chdir("../../..")
         # generate a random file to mark that the vendors are installed
         open("../vendor/.installed", "w").close()
         # StyleGAN2
