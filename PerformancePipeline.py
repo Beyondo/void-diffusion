b7f733af53d032c416ab9694324327a47573072d
Beyondo <58893646+Beyondo@users.noreply.github.com>
2024-01-04 23:35:33 +0200

    Update PerformancePipeline.py

--- PerformancePipeline.py
+++ PerformancePipeline.py
@@ -4,7 +4,7 @@ import torch
 
 def from_pretrained(model_name, safety_checker=None):
     torch.set_default_dtype(torch.float16)
-    rev = "diffusers-115k" if  model_name == "naclbit/trinart_stable_diffusion_v2" or model_name == "SG161222/Realistic_Vision_V2.0" else "fp16"
+    rev = "diffusers-115k" if  model_name == "naclbit/trinart_stable_diffusion_v2" else "" if model_name == "SG161222/Realistic_Vision_V2.0" else "fp16"
     pipe = None
     print('Chosen revision: %s' % rev)
     try:
