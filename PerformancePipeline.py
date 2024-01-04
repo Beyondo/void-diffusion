d3eb3e509b9c7c089beab7f001e69d8324debdd7
Beyondo <58893646+Beyondo@users.noreply.github.com>
2024-01-04 23:35:54 +0200

    Update PerformancePipeline.py

--- PerformancePipeline.py
+++ PerformancePipeline.py
@@ -6,7 +6,6 @@ def from_pretrained(model_name, safety_checker=None):
     torch.set_default_dtype(torch.float16)
     rev = "diffusers-115k" if  model_name == "naclbit/trinart_stable_diffusion_v2" else "" if model_name == "SG161222/Realistic_Vision_V2.0" else "fp16"
     pipe = None
-    print('Chosen revision: %s' % rev)
     try:
         pipe = StableDiffusionPipeline.from_pretrained(model_name, revision=rev, torch_dtype=torch.float16, safety_checker=safety_checker)
         pipe.to("cuda")
