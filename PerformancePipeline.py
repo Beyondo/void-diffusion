6100cf1cb2a3cfaf7ffa20077998f3b7c92168c2
Beyondo <58893646+Beyondo@users.noreply.github.com>
2024-01-04 22:52:44 +0200

    .

--- PerformancePipeline.py
+++ PerformancePipeline.py
@@ -1,9 +1,10 @@
 # PerformancePipeline Or PP for convenience (Hahah- ok, I'm sorry)
 from diffusers import StableDiffusionPipeline
 import torch
+
 def from_pretrained(model_name, safety_checker=None):
     torch.set_default_dtype(torch.float16)
-    rev = "diffusers-115k" if  model_name == "naclbit/trinart_stable_diffusion_v2" else "fp16"
+    rev = "diffusers-115k" if  model_name == "naclbit/trinart_stable_diffusion_v2" or model_name == "SG161222/Realistic_Vision_V2.0" else "fp16"
     pipe = None
     try:
         pipe = StableDiffusionPipeline.from_pretrained(model_name, revision=rev, torch_dtype=torch.float16, safety_checker=safety_checker)
