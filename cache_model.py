6330c6d18ff6e57807eb31bd362f1903df5ea378
Beyondo <58893646+Beyondo@users.noreply.github.com>
2024-01-05 01:12:29 +0200

    Update cache_model.py

--- cache_model.py
+++ cache_model.py
@@ -1,8 +1,10 @@
 import PerformancePipeline
 import argparse
+from IPython.display import clear_output
 parser = argparse.ArgumentParser()
 parser.add_argument('--model_name', type=str, default='SG161222/Realistic_Vision_V2.0', help='model name')
 args = parser.parse_args()
 model = PerformancePipeline.from_pretrained(args.model_name)
 del model
+clear_output()
 print("Cached model %s." % args.model_name)
